e5debb19e23cd271efa317146b7d9b1a18400058
Nikita Feldbush <nikita@feldbush.su>
2023-10-21 23:59:19 +0300

    image_fixed_else_worth

--- lab2/main.py
+++ lab2/main.py
@@ -1,6 +1,8 @@
 from PIL import Image
 import subprocess
 import random
+import numpy as np
+
 rel_path = "leasing.txt"
 hsh = subprocess.run(["C:/Program Files/Git/usr/bin/openssl.exe", "dgst",
                      "-sha1", rel_path], stdout=subprocess.PIPE).stdout.decode().strip()
@@ -26,48 +28,34 @@ def keygen(file_width, file_height, sha_len):
 
 
 def read_bmp_pixels_rgb(file_path):
-    with open(file_path, 'rb') as bmp_file:
-        binary_data = bmp_file.read()
-
-    pixel_data = []
-
-    if len(binary_data)%3==0:
-        start=0
-    else:
-        start=122
-
-    for i in range(start, len(binary_data), 3):
-        r, g, b = binary_data[i], binary_data[i + 1], binary_data[i + 2]
-        r_bits = format(r, '08b')
-        g_bits = format(g, '08b')
-        b_bits = format(b, '08b')
-        pixel_data.append([r_bits, g_bits, b_bits])
-
-    return pixel_data
+    img = Image.open(file_path)
+    pixel_data = list(img.getdata())
+    binary_pixel_data = [[format(pixel[0], '08b'), format(pixel[1], '08b'), format(pixel[2], '08b')] for pixel in pixel_data]
+    return binary_pixel_data
 
 
 def create_image_from_pixels(pixel_data, width, height, file_path):
     img = Image.new('RGB', (width, height))
     pixels = img.load()
-
     for y in range(height):
         for x in range(width):
             r, g, b = pixel_data[y * width + x]
-            pixels[x, height - y - 1] = (int(r, 2), int(g, 2), int(b, 2))
+            pixels[x,  y ] = (int(r, 2), int(g, 2), int(b, 2))
     img.save(file_path)
-
 
 def LSB_Replacement(file_width, file_height, sha, pixel_data):
     key = keygen(file_width, file_height, sha)
+    test=[]
     # Ключ хранит в себе значения переменных pixel_data в которых хранятся хеш значения. Слева направа.
     for i in range(len(key)):
-        # print(pixel_data[key[i][0]][key[i][1]])
-        # print(int(sha[i]))
         pixel_data[key[i][0]][key[i][1]] = bin(
             (int(pixel_data[key[i][0]][key[i][1]], 2) & ~1) | int(sha[i]))[2:]  # Перевожу значения в инт чтобы было более понятно, что происходит.
-        # print(pixel_data[key[i][0]][key[i][1]])
+        test.append(pixel_data[key[i][0]])
     create_image_from_pixels(pixel_data, file_width,
                              file_height, output_file_path)
+    new_file= read_bmp_pixels_rgb(output_file_path)
+    delta = set(map(tuple, new_file))
+    print(test==np.array([x for x in pixel_data if tuple(x) not in delta]))
 
 
 def return_sha1_hash(pixel_data):
@@ -75,20 +63,19 @@ def return_sha1_hash(pixel_data):
     with open("key.txt", "r") as f:
         key= f.readline()
     key = eval(key)
-    for i in range(len(key)):
-        a = bin(int(pixel_data[key[i][0]][key[i][1]], 2) & 1)
+    for k in range(len(key)):
+        a = pixel_data[key[k][0]][key[k][1]]
         a = a[len(a)-1:]
         fin+=str(a)
     return(fin)
 path = "28.bmp"
 a = read_bmp_pixels_rgb(path)
 
+
 width = 431
 height = 378
 output_file_path = 'output.bmp'
 new_file= read_bmp_pixels_rgb(output_file_path)
-# LSB_Replacement(width, height, hsh, a)
-# keygen(width,height,hsh)
-
+LSB_Replacement(width, height, hsh, a)
 print(return_sha1_hash(new_file))
 print(hsh)
